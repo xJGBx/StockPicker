b964eeaa637b809e3a53bd12889924c5f594da33
xJGBx <jgboyd2004@gmail.com>
2023-07-24 22:21:40 -0400

    Add finish

--- stockpicker.rb
+++ stockpicker.rb
@@ -1,7 +1,7 @@
 input = [17,3,6,9,15,8,6,1,10]
 
 def stock_picker(prices)
-    results = prices.each_with_index.to_a.combination(2).max_by{|buy,sell| sell[0] - buy[0].map{|price, i| i}}
-end 
+  results = prices.each_with_index.to_a.combination(2).max_by{|buy,sell| sell[0]-buy[0]}.map{|price, i| i}
+end
 
 puts(stock_picker(input))
